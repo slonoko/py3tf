08c29c68d650b04b6ee7a79786725050b6d2a9cd
Elie Khoury <elie.kh@gmail.com>
2019-06-10 10:15:49 +0200

    finished ch4

--- ch4.py
+++ ch4.py
@@ -6,7 +6,7 @@
 
 import pandas as pd
 
-data = pd.read_csv('../dummy_data.csv')
+data = pd.read_csv('dummy_data.csv')
 data
 
 
@@ -100,9 +100,85 @@ X_train_abs = abssc.fit_transform(X_train)
 X_test_abs = abssc.transform(X_test)
 
 X_train_abs
+#%%
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression(penalty= 'l1', C=1.0, solver='liblinear', multi_class='auto')
+lr.fit(X_train_std, y_train)
+print('training accuracy', lr.score(X_train_std, y_train))
+print('test accuracy', lr.score(X_test_std, y_test))
+#%%
+import matplotlib.pyplot as plt
 
+fig = plt.figure()
+ax = plt.subplot(111)
 
+colors = ['blue', 'green', 'red', 'cyan',
+         'magenta', 'yellow', 'black',
+         'pink', 'lightgreen', 'lightblue',
+         'gray', 'indigo', 'orange']
+weights, params = [], []
+for c in np.arange(-4., 6.):
+    lr = LogisticRegression(penalty='l1', 
+                            C=10.**c,
+                            random_state=0, solver='liblinear', multi_class='auto')
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
 
+weights = np.array(weights)
 
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+             label=df_wine.columns[column + 1],
+             color=color)
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center',
+          bbox_to_anchor=(1.38, 1.03),
+          ncol=1, fancybox=True)
+plt.show()
+
+#%%
+
+from sklearn.ensemble import RandomForestClassifier
+
+feat_labels = df_wine.columns[1:]
+
+forest = RandomForestClassifier(n_estimators=500,
+                               random_state=1)
+forest.fit(X_train, y_train)
+importances = forest.feature_importances_
+
+indices = np.argsort(importances)[::-1]
+
+for f in range(X_train.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30,
+                            feat_labels[indices[f]],
+                            importances[indices[f]]))
+plt.title('Feature Importance')
+plt.bar(range(X_train.shape[1]),
+        importances[indices],
+        align='center')
+plt.xticks(range(X_train.shape[1]),
+           feat_labels[indices], rotation=90)
+plt.xlim([-1, X_train.shape[1]])
+plt.tight_layout()
+plt.show()
+#%%
+from sklearn.feature_selection import SelectFromModel
+
+sfm = SelectFromModel(forest, threshold=0.1, prefit=True)
+X_selected = sfm.transform(X_train)
+print('Number of features that meet this threshold criterion:', 
+      X_selected.shape[1])
+
+for f in range(X_selected.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30, 
+                            feat_labels[indices[f]], 
+                            importances[indices[f]]))
 
 #%%
