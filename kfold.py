20b1786a7207dfaa69843ce5ecd9ed936dfe1d2d
Elie Khoury <elie.kh@gmail.com>
2019-06-28 22:14:41 +0200

    using grid CV

--- kfold.py
+++ kfold.py
@@ -16,9 +16,15 @@ from sklearn.pipeline import make_pipeline
 pipe_lr = make_pipeline(StandardScaler(),
                         PCA(n_components=2),
                         LogisticRegression(random_state=1))
-pipe_lr.fit(X_train, y_train)
-y_pred = pipe_lr.predict(X_test)
-print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
+#pipe_lr.fit(X_train, y_train)
+#y_pred = pipe_lr.predict(X_test)
+#print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
+param_range=[0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+param_grid2=[{'logisticregression__C': param_range}]
+gs2 = GridSearchCV(estimator=pipe_lr, param_grid= param_grid2, scoring="accuracy", cv=10, n_jobs=-1)
+gs2= gs2.fit(X_train, y_train)
+print(gs2.best_score_)
+print(gs2.best_params_)
 #%%
 import numpy as np 
 from sklearn.model_selection import StratifiedKFold
@@ -48,5 +54,23 @@ scores = cross_val_score(estimator=pipe_lr,
 print('CV accuracy scores: %s' % scores)
 print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores),
       np.std(scores)))
+
+#%%
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+
+
+pipe_svc = make_pipeline(StandardScaler(),SVC(random_state=1))
+param_range=[0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+param_grid= [{'svc__C': param_range,
+               'svc__kernel': ['linear']},
+              {'svc__C': param_range,
+                   'svc__gamma': param_range,
+               'svc__kernel': ['rbf']}]
+
+gs = GridSearchCV(estimator=pipe_svc, param_grid= param_grid, scoring="accuracy", cv=10, n_jobs=-1)
+gs= gs.fit(X_train, y_train)
+print(gs.best_score_)
+print(gs.best_params_)
 
 #%%
