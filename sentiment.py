3e5cc226bd1bb430f18fd932a78d218961747a6e
Elie Khoury <elie.kh@gmail.com>
2019-06-29 20:47:10 +0200

    added sentiment analysis

---
+++ sentiment.py
@@ -0,0 +1,134 @@
+#%% Importing libraries
+
+import warnings
+warnings.simplefilter('ignore')
+!pip install --user numpy
+!pip install --user scipy
+!pip install --user scikit-learn
+!pip install --user matplotlib
+!pip install --user pandas
+!pip install --user tensorflow
+!pip install --user sklearn
+!pip install --user pyprind
+
+#%% Imports
+
+import tarfile
+import pyprind
+import os
+import urllib.request
+import pandas as pd
+
+#%% Downloading the sentiment data
+
+urllib.request.urlretrieve('http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz', 'aclImdb.tar.gz')
+with tarfile.open('aclImdb.tar.gz','r:gz') as tar:
+    tar.extractall()
+
+#%% Preparing the data
+basepath = 'aclImdb'
+
+labels = {'pos':1, 'neg':0}
+pbar = pyprind.ProgBar(50000)
+
+df = pd.DataFrame()
+for s in ('test', 'train'):
+    for l in ('pos','neg'):
+        path = os.path.join(basepath,s,l)
+        for file in sorted(os.listdir(path)):
+            with open(os.path.join(path, file), 'r',encoding='utf-8') as infile:
+                txt = infile.read()
+                df = df.append([[txt, labels[l]]], ignore_index=True)
+                pbar.update()
+df.columns = ['review', 'sentiment']
+
+#%% Storing in csv file
+import numpy as np
+
+np.random.seed(0)
+df = df.reindex(np.random.permutation(df.index))
+df.to_csv('movie_data.csv', index=False, encoding='utf-8')
+
+
+#%% Cleaningup files
+import shutil
+
+if os.path.exists('aclImdb.tar.gz'):
+    os.remove('aclImdb.tar.gz')
+if os.path.exists('aclImdb'):
+    shutil.rmtree('aclImdb')
+
+#%%
+df =pd.read_csv('movie_data.csv')
+df.head()
+
+#%% Clean up function
+import re
+from nltk.stem.porter import PorterStemmer
+import nltk
+from nltk.corpus import stopwords
+
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
+                           text)
+    text = (re.sub('[\W]+', ' ', text.lower()) +
+            ' '.join(emoticons).replace('-', ''))
+    return text
+
+def tokenizer(text):
+    return text.split()
+
+porter = PorterStemmer()
+
+def tokenizer_porter(text):
+    return [porter.stem(word) for word in text.split()]
+
+nltk.download('stopwords')
+stop = stopwords.words('english')
+
+df['review'] = df['review'].apply(preprocessor)
+#%%
+X_train = df.loc[:25000, 'review'].values
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = df.loc[25000:, 'review'].values
+y_test = df.loc[25000:, 'sentiment'].values
+
+#%%
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+tfidf = TfidfVectorizer(strip_accents=None,
+                        lowercase=False,
+                        preprocessor=None)
+param_grid = [{'vect__ngram_range': [(1,1)],
+              'vect__stop_words': [stop, None],
+              'vect__tokenizer': [tokenizer,
+                                  tokenizer_porter],
+              'clf__penalty': ['l1', 'l2'],
+              'clf__C': [1.0, 10.0, 100.0]},
+            {'vect__ngram_range': [(1,1)],
+              'vect__stop_words': [stop, None],
+              'vect__tokenizer': [tokenizer,
+                                  tokenizer_porter],
+              'vect__use_idf':[False],
+              'vect__norm':[None],
+              'clf__penalty': ['l1', 'l2'],
+              'clf__C': [1.0, 10.0, 100.0]}
+            ]
+lr_tfidf = Pipeline([('vect', tfidf),
+                    ('clf',
+                     LogisticRegression(random_state=0))])
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
+                          scoring='accuracy',
+                          cv=5, verbose=1,
+                          n_jobs=-1)
+gs_lr_tfidf.fit(X_train, y_train)
+
+#%%
+print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
+print('CV Accuracy: %.3f'% gs_lr_tfidf.best_score_)
+clf = gs_lr_tfidf.best_estimator_
+print('Test Accuracy: %.3f' % clf.score(X_test, y_test))
