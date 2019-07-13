e36ccb5e25d2d99997c73cd35ba2f7a623b2d871
Elie Khoury <elie.kh@gmail.com>
2019-07-13 21:16:41 +0200

    added web app

---
+++ mnist_ws/ws.py
@@ -0,0 +1,29 @@
+import random
+import string
+from tensorflow.keras.models import load_model
+import cherrypy
+import numpy as np
+
+
+class MyWebService(object):
+    @cherrypy.expose
+    def index(self):
+        return open("./canvas.html")
+
+    @cherrypy.expose
+    def predict(self, nb_field=""):
+        model = load_model('mnist_model.h5')
+        arr_test = nb_field.split(',')
+        __input = np.array(arr_test, dtype=float)
+        __input = np.expand_dims(__input, axis=0)
+        y_test_pred = model.predict_classes(__input, verbose=1)
+
+        return y_test_pred
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'tools.sessions.on': True
+        }
+    }
+    cherrypy.quickstart(MyWebService(), '/', conf)
