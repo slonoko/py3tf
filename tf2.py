8a0d52a2f7bf50902555037bdabe04602ae989ee
Elie Khoury <elie.kh@gmail.com>
2019-08-08 21:08:43 +0200

    using cifar10 with keras

--- tf2.py
+++ tf2.py
@@ -42,8 +42,72 @@ with opt_graph.as_default():
 with tf.Session(graph=opt_graph) as sess2:
     sess2.run(init)
     print(f'starting at x: {sess2.run(x)}, log(x)^2: {sess2.run(log_x_squared)}')
-    for step in range(10):
+    for step in range(20):
         sess2.run(train)
         print(f'step {step}, with x: {sess2.run(x)}, log(x)^2: {sess2.run(log_x_squared)}')
 
+#%%
+import numpy as np
+import os
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import Adam, SGD
+from keras.utils import np_utils
+
+np.random.seed(100)
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+
+
+#%%
+x_train = x_train.reshape(50000, 3072)
+x_test = x_test.reshape(10000,3072)
+
+x_train = (x_train - np.mean(x_train))/np.std(x_train)
+x_test = (x_test - np.mean(x_test))/np.std(x_test)
+
+#%%
+labels = 10
+y_train = np_utils.to_categorical(y_train, labels)
+y_test = np_utils.to_categorical(y_test, labels)
+
+#%%
+model = Sequential()
+model.add(Dense(512, input_shape=(3072,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.4))
+model.add(Dense(120))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(labels))
+model.add(Activation('sigmoid'))
+
+#%%
+adam = Adam(0.01)
+sgd = SGD(0.01)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+#%%
+model.fit(x_train, y_train, batch_size=1000, nb_epoch=50, validation_data=(x_test,y_test))
+#%%
+score = model.evaluate(x_test, y_test, verbose=0)
+print(f'Test accuracy is {score[1]}')
+
+#%%
+model.predict_classes(x_test)
+
+#%%
+model.save('model.h5')
+jsonModel = model.to_json()
+model.save_weights('model_weights.h5')
+
+#%%
+model.summary()
+
+#%%
+from keras.callbacks import EarlyStopping
+early_stopping_monitor = EarlyStopping(patience=2)
+model.fit(x_train, y_train, batch_size=1000, nb_epoch=10, validation_data=(x_test,y_test), callbacks=[early_stopping_monitor])
+#%%
+model.output_shape
 #%%
