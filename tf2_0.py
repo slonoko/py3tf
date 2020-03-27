e5e7f17ff01d710629d4932658aa9d135597cc17
Elie Khoury <elie.kh@gmail.com>
2020-03-27 21:38:51 +0100

    fixing some imports

--- tf2_0.py
+++ tf2_0.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
-from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.python.client import device_lib
 
 print(f'Tensorflow version {tf.__version__}\n')
 print(device_lib.list_local_devices())
 
-W = tf.Variable(tf.ones(shape=(2,2)), name="W")
-b = tf.Variable(tf.zeros(shape=(2,2)), name="b")
+W = tf.Variable(tf.ones(shape=(2, 2)), name="W")
+b = tf.Variable(tf.zeros(shape=(2, 2)), name="b")
+
 
 @tf.function
 def model(x):
     return W * x + b
 
-out_a = model([1,0])
-print(out_a)
 
+out_a = model([1, 0])
+print(out_a)
