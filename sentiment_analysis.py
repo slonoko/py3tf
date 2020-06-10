cc4beb7a14aaa2d3dce0a856e3406fe4e027a0a7
Elie Khoury <elie.kh@gmail.com>
2020-06-10 14:55:31 +0200

    loading model

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,19 +1,45 @@
 import tensorflow as tf
+import os
 import numpy as np
 from tensorflow.keras import datasets, layers, models, preprocessing
 import tensorflow_datasets as tfds
 
-max_len = 200
-n_words = 10000
+max_len = 500
+n_words = 13000
 dim_embedding = 256
 EPOCHS = 10
-BATCH_SIZE = 1000
+BATCH_SIZE = 500
+model_dir = '.data/imdb_model'
+
+pos_review = "Joaquin Phoenix gives a tour de force performance, fearless and stunning in its emotional depth and " \
+            "physicality. It's impossible to talk about this without referencing Heath Ledger's Oscar-winning " \
+            "performance from The Dark Knight, widely considered the definitive live-action portrayal of the Joker, " \
+            "so let's talk about it. The fact is, everyone is going to be stunned by what Phoenix accomplishes, " \
+            "because it's what many thought impossible - a portrayal that matches and potentially exceeds that of The " \
+            "Dark Knight's Clown Prince of Crime "
+
+neg_review = "Once again Mr. Costner has dragged out a movie for far longer than necessary. Aside from the terrific " \
+             "sea rescue sequences, of which there are very few I just did not care about any of the characters. Most " \
+             "of us have ghosts in the closet, and Costner's character are realized early on, and then forgotten " \
+             "until much later, by which time I did not care. The character we should really care about is a very " \
+             "cocky, overconfident Ashton Kutcher. The problem is he comes off as kid who thinks he's better than " \
+             "anyone else around him and shows no signs of a cluttered closet. His only obstacle appears to be " \
+             "winning over Costner. Finally when we are well past the half way point of this stinker, Costner tells " \
+             "us all about Kutcher's ghosts. We are told why Kutcher is driven to be the best with no prior inkling " \
+             "or foreshadowing. No magic here, it was all I could do to keep from turning it off an hour in. "
 
 gpus = tf.config.experimental.list_physical_devices("GPU")
 
 if len(gpus) > 0:
     print("Using a GPU ...")
     tf.config.experimental.set_memory_growth(gpus[0], True)
+
+
+def prepare_embedding(review):
+    encoded_doc = tf.keras.preprocessing.text.one_hot(review, n_words)
+    padded_doc = preprocessing.sequence.pad_sequences([encoded_doc], maxlen=max_len, padding="post")
+    return padded_doc
+
 
 def load_data():
     # load data
@@ -46,17 +72,26 @@ def build_model():
 model = build_model()
 model.summary()
 
-callbacks = [tf.keras.callbacks.TensorBoard(log_dir='log_imdb')]
+callbacks = [tf.keras.callbacks.TensorBoard(log_dir='.data/log_imdb'),
+             tf.keras.callbacks.ModelCheckpoint(model_dir)]
 
 model.compile(optimizer=tf.optimizers.Adam(), loss=tf.losses.BinaryCrossentropy(), metrics=["accuracy"])
 
-score = model.fit(
-    X_train,
-    y_train,
-    epochs=EPOCHS,
-    batch_size=BATCH_SIZE,
-    validation_data=(X_test, y_test)
-)
-score = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE)
-print("\nTest score:", score[0])
-print("Test accuracy:", score[1])
+if os.path.exists(model_dir):
+    model.load_weights(f'{model_dir}/variables/variables')
+else:
+    model.fit(
+        X_train,
+        y_train,
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
+        validation_data=(X_test, y_test),
+        callbacks=callbacks
+    )
+    score = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE)
+    print("\nTest score:", score[0])
+    print("Test accuracy:", score[1])
+
+pos_result = model.predict(prepare_embedding(pos_review))
+neg_result = model.predict(prepare_embedding(neg_review))
+print(pos_result, neg_result)
